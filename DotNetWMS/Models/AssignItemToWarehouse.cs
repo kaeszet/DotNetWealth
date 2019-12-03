7c5ec44a5e3c35402368be678096aa9ba05a59ee
KamilSzydlowski <43911771+KamilSzydlowski@users.noreply.github.com>
2019-12-04 00:02:40 +0100

    first commit

---
+++ DotNetWMS/Models/AssignItemToWarehouse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetWMS.Models
+{
+    public class AssignItemToWarehouse
+    {
+        public int WarehouseId { get; set; }
+        public Employee Warehouse { get; set; }
+        public int ItemId { get; set; }
+        public ItemState ItemState { get; set; }
+        public Item Item { get; set; }
+    }
+}
---
+++ DotNetWMS/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetWMS.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Pesel { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+    }
+}
