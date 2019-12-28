40337559ef6c3bada4c96c1dc6ec2b126a1f3844
KamilSzydlowski <43911771+KamilSzydlowski@users.noreply.github.com>
2019-12-28 19:50:13 +0100

    exception handlers

---
+++ DotNetWMS/Controllers/ErrorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetWMS.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Strona pod podanym adresem nie istnieje!";
+                    ViewBag.Path = statusCodeResult.OriginalPath;
+                    ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    break;
+            }
+
+            return View("NotFound");
+        }
+        [Route("Error")]
+        public IActionResult GlobalExceptionHandler()
+        {
+            var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
+            ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
+            ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+            ViewBag.ContactEmail = "Skontaktuj się: kamil.szydlowski@microsoft.wsei.edu.pl";
+
+            return View();
+        }
+    }
+}
