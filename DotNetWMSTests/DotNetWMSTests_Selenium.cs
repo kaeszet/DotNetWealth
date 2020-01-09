03a74da24f17d7c74797b276c178ad05dd2acfa4
mjKeszely <kamil.szydlowski@microsoft.wsei.edu.pl>
2020-01-09 19:03:30 +0100

    Selenium tests

--- DotNetWMSTests/DotNetWMSTests_Selenium.cs
+++ DotNetWMSTests/DotNetWMSTests_Selenium.cs
@@ -10,6 +10,7 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using DotNetWMSTests.Selenium_test;
 
 namespace DotNetWMSTests
 {
@@ -18,16 +19,15 @@ namespace DotNetWMSTests
     {
 
         private IWebDriver driver;
-        private StringBuilder verificationErrors;
         private const string URI = "https://localhost:44387/";
-        private bool acceptNextAlert = true;
+        private string invalidCredentials = "(.//*[normalize-space(text()) and normalize-space(.)='Zarejestruj się'])[1]/following::li[1]";
         private int count;
 
         [SetUp]
         public void SetupTest()
         {
             driver = new ChromeDriver();
-            verificationErrors = new StringBuilder();
+            
         }
 
         [TearDown]
@@ -41,44 +41,30 @@ namespace DotNetWMSTests
             {
                 // Ignore errors if unable to close the browser
             }
-            Assert.AreEqual("", verificationErrors.ToString());
         }
 
         [Test]
         public void Employee_LoginAddEditRemoveObject_PathWithCorrectData()
         {
-            //TODO tryfind login
-            //Registration();
+            
             driver.Navigate().GoToUrl(URI);
-            //Login();
+            Login();
             driver.FindElement(By.XPath("//a/i")).Click();            
             driver.FindElement(By.LinkText("Dodaj użytkownika")).Click();
-            driver.FindElement(By.Id("Name")).Click();
-            driver.FindElement(By.Id("Name")).Clear();
             driver.FindElement(By.Id("Name")).SendKeys("Jessica");
-            driver.FindElement(By.Id("Surname")).Clear();
             driver.FindElement(By.Id("Surname")).SendKeys("Testowa");
-            driver.FindElement(By.Id("Pesel")).Clear();
             driver.FindElement(By.Id("Pesel")).SendKeys("12345678901");
-            driver.FindElement(By.Id("DepartmentId")).Click();
             new SelectElement(driver.FindElement(By.Id("DepartmentId"))).SelectByText("Księgowy/a");
-            driver.FindElement(By.Id("DepartmentId")).Click();
-            driver.FindElement(By.Id("Street")).Click();
-            driver.FindElement(By.Id("Street")).Clear();
             driver.FindElement(By.Id("Street")).SendKeys("św. Filipa");
-            driver.FindElement(By.Id("ZipCode")).Clear();
             driver.FindElement(By.Id("ZipCode")).SendKeys("30-000");
-            driver.FindElement(By.Id("City")).Clear();
             driver.FindElement(By.Id("City")).SendKeys("Kraków");
             driver.FindElement(By.XPath("//input[@value='Create']")).Click();
             count = driver.FindElements(By.XPath("//*[@class='table']/tbody/tr")).Count;
             driver.FindElement(By.XPath($"(.//*[normalize-space(text()) and normalize-space(.)='Kraków'])[{count}]/following::a[1]")).Click();
-            driver.FindElement(By.Id("Pesel")).Click();
-            driver.FindElement(By.Id("Pesel")).Clear();
             driver.FindElement(By.Id("Pesel")).SendKeys("12345678902");
-            driver.FindElement(By.Id("DepartmentId")).Click();
+            //driver.FindElement(By.Id("DepartmentId")).Click();
             new SelectElement(driver.FindElement(By.Id("DepartmentId"))).SelectByText("Sprzedawca");
-            driver.FindElement(By.Id("DepartmentId")).Click();
+            //driver.FindElement(By.Id("DepartmentId")).Click();
             driver.FindElement(By.XPath("//input[@value='Save']")).Click();
             driver.FindElement(By.XPath($"//tr[{count}]/td[8]/a[2]/i")).Click();
             driver.FindElement(By.LinkText("Back to List")).Click();
@@ -90,62 +76,42 @@ namespace DotNetWMSTests
         public void Employee_LoginAddEditRemoveObject_PathWithIncorrectData()
         {
             driver.Navigate().GoToUrl(URI);
+            Login();
             driver.FindElement(By.XPath("//a/i")).Click();
             
             driver.FindElement(By.LinkText("Dodaj użytkownika")).Click();
-            driver.FindElement(By.Id("Name")).Click();
-            driver.FindElement(By.Id("Name")).Clear();
             driver.FindElement(By.Id("Name")).SendKeys("Janusz");
-            driver.FindElement(By.Id("Surname")).Click();
-            driver.FindElement(By.Id("Surname")).Clear();
             driver.FindElement(By.Id("Surname")).SendKeys("Testowa");
-            driver.FindElement(By.Id("Pesel")).Click();
-            driver.FindElement(By.Id("Pesel")).Clear();
             driver.FindElement(By.Id("Pesel")).SendKeys("1234567");
-            driver.FindElement(By.Id("DepartmentId")).Click();
             new SelectElement(driver.FindElement(By.Id("DepartmentId"))).SelectByText("Sprzedawca");
-            driver.FindElement(By.Id("DepartmentId")).Click();
-            driver.FindElement(By.Id("Street")).Click();
-            driver.FindElement(By.Id("Street")).Clear();
             driver.FindElement(By.Id("Street")).SendKeys("św. Filipa");
-            driver.FindElement(By.Id("ZipCode")).Clear();
             driver.FindElement(By.Id("ZipCode")).SendKeys("30000");
-            driver.FindElement(By.Id("City")).Clear();
             driver.FindElement(By.Id("City")).SendKeys("Kraków");
             driver.FindElement(By.XPath("//input[@value='Create']")).Click();
 
             Assert.IsTrue(IsElementPresent(By.Id("Pesel-error")));
             Assert.IsTrue(IsElementPresent(By.Id("ZipCode-error")));
 
-            driver.FindElement(By.Id("ZipCode")).Click();
             driver.FindElement(By.Id("ZipCode")).Clear();
-            driver.FindElement(By.Id("ZipCode")).SendKeys("30-000");
-            driver.FindElement(By.Id("Pesel")).Click();
             driver.FindElement(By.Id("Pesel")).Clear();
+            driver.FindElement(By.Id("ZipCode")).SendKeys("30-000");
             driver.FindElement(By.Id("Pesel")).SendKeys("12345670000");
             driver.FindElement(By.XPath("//input[@value='Create']")).Click();
             count = driver.FindElements(By.XPath("//*[@class='table']/tbody/tr")).Count;
             driver.FindElement(By.XPath($"(.//*[normalize-space(text()) and normalize-space(.)='Kraków'])[{count}]/following::a[1]")).Click();
             driver.FindElement(By.XPath("//body")).Click();
             driver.FindElement(By.Id("Name")).Clear();
-            driver.FindElement(By.Id("Name")).SendKeys("Grażyna");
-            driver.FindElement(By.Id("Pesel")).Click();
             driver.FindElement(By.Id("Pesel")).Clear();
+            driver.FindElement(By.Id("Name")).SendKeys("Grażyna");
             driver.FindElement(By.Id("Pesel")).SendKeys("123456700");
-            driver.FindElement(By.Id("DepartmentId")).Click();
-            new SelectElement(driver.FindElement(By.Id("DepartmentId"))).SelectByText("Księgowy/a");
-            driver.FindElement(By.Id("DepartmentId")).Click();
-            driver.FindElement(By.Id("ZipCode")).Click();
+            new SelectElement(driver.FindElement(By.Id("DepartmentId"))).SelectByText("Księgowy/a")
             driver.FindElement(By.Id("ZipCode")).Clear();
             driver.FindElement(By.Id("ZipCode")).SendKeys("aa000");
-            //driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Miasto'])[1]/following::div[1]")).Click();
             driver.FindElement(By.XPath("//input[@value='Save']")).Click();
 
             Assert.IsTrue(IsElementPresent(By.Id("Pesel-error")));
             Assert.IsTrue(IsElementPresent(By.Id("ZipCode-error")));
 
-            //driver.FindElement(By.Id("ZipCode")).Clear();
-            //driver.FindElement(By.Id("ZipCode")).SendKeys("30-000");
             driver.FindElement(By.Id("Pesel")).Clear();
             driver.FindElement(By.Id("Pesel")).SendKeys("12345670011");
             driver.FindElement(By.Id("Surname")).Clear();
@@ -159,17 +125,9 @@ namespace DotNetWMSTests
             Assert.IsTrue(IsElementPresent(By.Id("City-error")));
             Assert.IsTrue(IsElementPresent(By.Id("ZipCode-error")));
 
-            driver.FindElement(By.Id("Surname")).Click();
-            driver.FindElement(By.Id("Surname")).Clear();
             driver.FindElement(By.Id("Surname")).SendKeys("Testowa");
-            //driver.FindElement(By.Id("Street")).Click();
-            //driver.FindElement(By.Id("Surname")).Clear();
-            //driver.FindElement(By.Id("Surname")).SendKeys("Testowy");
-            driver.FindElement(By.Id("Street")).Clear();
             driver.FindElement(By.Id("Street")).SendKeys("św. Filipa");
-            driver.FindElement(By.Id("ZipCode")).Clear();
             driver.FindElement(By.Id("ZipCode")).SendKeys("30-000");
-            driver.FindElement(By.Id("City")).Clear();
             driver.FindElement(By.Id("City")).SendKeys("Kraków");
             driver.FindElement(By.XPath("//input[@value='Save']")).Click();
             driver.FindElement(By.XPath($"//tr[{count}]/td[8]/a[2]/i")).Click();
@@ -179,47 +137,75 @@ namespace DotNetWMSTests
             driver.FindElement(By.XPath("//input[@value='Delete']")).Click();
 
             Assert.IsFalse(IsElementPresent(By.XPath($"(.//*[normalize-space(text()) and normalize-space(.)='Kraków'])[{count}]/following::a[3]")));
-
-            driver.FindElement(By.LinkText(".NetFortune")).Click();
+;
         }
         [Test]
+        public void AccountLogin_UseInvalidLoginAndPassword_ReturnLoginFailInfo()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.GoToPage();
+            loginPage.UserName.SendKeys("InvalidLogin");
+            loginPage.Password.SendKeys("InvalidPassword");
+            loginPage.Submit.Click();
+            Assert.IsTrue(loginPage.LoginFailInfo.Displayed);
+
+        }
+        [Test]
+        public void AccountLogin_UseValidLoginAndPassword_ReturnSuccess()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.GoToPage();
+            loginPage.UserName.SendKeys("TestoJan9012");
+            loginPage.Password.SendKeys("Test123!");
+            loginPage.Submit.Click();
+            Assert.IsTrue(loginPage.LoginSuccessButton.Displayed && loginPage.LoginSuccessButton.Text == "Logout TestoJan9012");
+
+        }
+        [Test]
+        public void AccountRegistration_LeaveRequiredFieldsEmpty_ShowExceptionList()
+        {
+            RegisterPage registerPage = new RegisterPage(driver);
+            registerPage.GoToPage();
+            registerPage.Submit.Click();
+            Assert.IsTrue(registerPage.FirstExceptionOnList.Displayed);
+
+        }
+        [Test]
+        public void AccountRegistration_()
+        {
+            RegisterPage registerPage = new RegisterPage(driver);
+            registerPage.GoToPage();
+            registerPage.EmpId.SendKeys("InvalidEmpId");
+            registerPage.Submit.Click();
+            Assert.IsTrue(registerPage.EmpIdSpan.Displayed && registerPage.EmpIdSpan.Text == "Nieprawidłowy identyfikator!");
+
+        }
+
         public void Registration()
         {
             driver.Navigate().GoToUrl(URI);
             driver.FindElement(By.LinkText("Zarejestruj się")).Click();
-            driver.FindElement(By.Id("Name")).Click();
-            driver.FindElement(By.Id("Name")).Clear();
             driver.FindElement(By.Id("Name")).SendKeys("Janusz");
-            driver.FindElement(By.Id("City")).Clear();
             driver.FindElement(By.Id("City")).SendKeys("Kraków");
-            driver.FindElement(By.Id("Surname")).Click();
-            driver.FindElement(By.Id("Surname")).Clear();
             driver.FindElement(By.Id("Surname")).SendKeys("Testowy");
-            driver.FindElement(By.Id("EmployeeNumber")).Click();
-            driver.FindElement(By.Id("EmployeeNumber")).Clear();
             driver.FindElement(By.Id("EmployeeNumber")).SendKeys("123456789012");
-            driver.FindElement(By.Id("Email")).Click();
-            driver.FindElement(By.Id("Email")).Clear();
             driver.FindElement(By.Id("Email")).SendKeys("a@a.pl");
-            driver.FindElement(By.Id("Password")).Click();
-            driver.FindElement(By.Id("Password")).Clear();
             driver.FindElement(By.Id("Password")).SendKeys("Test123!");
-            driver.FindElement(By.Id("ConfirmPassword")).Clear();
             driver.FindElement(By.Id("ConfirmPassword")).SendKeys("Test123!");
             driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Potwierdź hasło'])[1]/following::button[1]")).Click();
         }
-        [Test]
+        
         public void Login()
         {
             driver.Navigate().GoToUrl(URI);
             driver.FindElement(By.LinkText("Zaloguj się")).Click();
-            driver.FindElement(By.Id("inputEmail")).Click();
-            driver.FindElement(By.Id("inputEmail")).Clear();
             driver.FindElement(By.Id("inputEmail")).SendKeys("TestoJan9012");
-            driver.FindElement(By.Id("inputPassword")).Click();
-            driver.FindElement(By.Id("inputPassword")).Clear();
             driver.FindElement(By.Id("inputPassword")).SendKeys("Test123!");
             driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Hasło'])[1]/following::button[1]")).Click();
+            if (IsElementPresent(By.XPath(invalidCredentials)))
+            {
+                Registration();
+            }
         }
         private bool IsElementPresent(By by)
         {
@@ -234,40 +220,6 @@ namespace DotNetWMSTests
             }
         }
 
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-
-        private string CloseAlertAndGetItsText()
-        {
-            try
-            {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
-        }
 
     }
 }
---
+++ DotNetWMSTests/Selenium_test/RegisterPage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetWMSTests.Selenium_test
+{
+    class RegisterPage
+    {
+        private IWebDriver driver;
+        public RegisterPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "Name")]
+        public IWebElement Name { get; set; }
+        [FindsBy(How = How.Id, Using = "Surname")]
+        public IWebElement Surname { get; set; }
+        [FindsBy(How = How.Id, Using = "EmployeeNumber")]
+        public IWebElement EmpId { get; set; }
+        [FindsBy(How = How.Id, Using = "City")]
+        public IWebElement Street { get; set; }
+        [FindsBy(How = How.Id, Using = "Email")]
+        public IWebElement Email { get; set; }
+        [FindsBy(How = How.Id, Using = "Password")]
+        public IWebElement Password { get; set; }
+        [FindsBy(How = How.Id, Using = "ConfirmPassword")]
+        public IWebElement ConfPassword { get; set; }
+        [FindsBy(How = How.XPath, Using = "(.//*[normalize-space(text()) and normalize-space(.)='Potwierdź hasło'])[1]/following::button[1]")]
+        public IWebElement Submit { get; set; }
+        [FindsBy(How = How.XPath, Using = "(.//*[normalize-space(text()) and normalize-space(.)='Rejestracja'])[1]/following::li[1]")]
+        public IWebElement FirstExceptionOnList { get; set; }
+        [FindsBy(How = How.XPath, Using = "(.//*[normalize-space(text()) and normalize-space(.)='Identyfikator'])[1]/following::span[1]")]
+        public IWebElement EmpIdSpan { get; set; }
+
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/Account/Register");
+        }
+        
+    }
+}
