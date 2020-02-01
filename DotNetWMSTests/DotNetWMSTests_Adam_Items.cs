8c0d3f199f8cd808672c57d8fc2363f8343430f5
Adam Żur <adam.zur@microsoft.wsei.edu.pl>
2020-02-01 13:59:27 +0100

    Add Test Adam

---
+++ DotNetWMSTests/DotNetWMSTests_Adam_Items.cs
@@ -0,0 +1,231 @@
+﻿using DotNetWMS.Controllers;
+using DotNetWMS.Data;
+using DotNetWMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace DotNetWMSTests
+{
+    public class DotNetWMSTests_Adam_Items : DotNetWMSTests_Base
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Model_CheckIsModelValidIfFilledWithLenghtException_ReturnFalse()
+        {
+
+            var emp = new Item() { Id = 4, Type = "Komputer osobisty_Komputer osobisty", Name = "Laptop", Producer = "Lenovo", Model = "X1 Carbon", ItemCode = "2", Quantity = 1, Units = 0, WarrantyDate = DateTime.Now, State = 0, EmployeeId = 2, WarehouseId = 2, ExternalId = 1 };
+            var isModelValid = TryValidate(emp, out _);
+            Assert.IsFalse(isModelValid);
+
+
+        }
+
+        [Test]
+        public void Model_CheckIsModelValidIfRequiredFieldAreNotFilled_ReturnFalse()
+        {
+            var emp = new Item();
+            var isModelValid = TryValidate(emp, out _);
+            Assert.IsFalse(isModelValid);
+
+        }
+        [Test]
+        public void Model_CheckIsModelValidIfRequiredFieldAreFilled_ReturnTrue()
+        {
+            var emp = new Item() { Id = 4, Type = "Komputer osobisty", Name = "Laptop", Producer = "Lenovo", Model = "X1 Carbon", ItemCode = "2", Quantity = 1, Units = 0, WarrantyDate = DateTime.Now, State = 0, EmployeeId = 2, WarehouseId = 2, ExternalId = 1 };
+            var isModelValid = TryValidate(emp, out _);
+            Assert.IsTrue(isModelValid);
+
+
+
+        
+        }
+        [Test]
+        public void CreateGet_GetCreateView_ReturnViewObjectWithoutInjectedData()
+        {
+
+            var controller = new ItemsController(_context);
+            var result = controller.Create() as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.IsNull(result.Model);
+
+
+        }
+        [Test]
+        public async Task CreatePost_AddNewRecordToDb_RecordAdded()
+        {
+            var _options = new DbContextOptionsBuilder<DotNetWMSContext>()
+               .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+               .Options;
+
+            DotNetWMSContext _context = new DotNetWMSContext(_options);
+            _context.Database.EnsureCreated();
+            Initialize(_context);
+
+            var emp = new Item() { Id = 4, Type = "Komputer osobisty", Name = "Laptop", Producer = "Lenovo", Model = "X1 Carbon", ItemCode = "2", Quantity = 1, Units = 0, WarrantyDate = DateTime.Now, State = 0, EmployeeId = 2, WarehouseId = 2, ExternalId = 1 };
+            var controller = new ItemsController(_context);
+            var result = await controller.Create(emp) as RedirectToActionResult;
+            Assert.IsTrue(result.ActionName == nameof(controller.Index));
+
+
+        }
+        [Test]
+        public async Task Details_CheckRecordWithExistingKey_ReturnViewResult()
+        {
+            var controller = new ItemsController(_context);
+            var result = await controller.Details(1) as ViewResult;
+            Assert.IsNotNull(result);
+
+
+        }
+        [Test]
+        public async Task Details_CheckRecordWithNotExistingKey_ReturnNull()
+        {
+
+            var controller = new ItemsController(_context);
+            var result = await controller.Details(99) as ViewResult;
+            Assert.IsNull(result);
+
+
+        }
+        [Test]
+        public async Task Details_CheckRecordWithNull_ReturnNotFoundResult()
+        {
+
+            var controller = new ItemsController(_context);
+            var result = await controller.Details(null);
+            Assert.That(result, Is.InstanceOf(typeof(NotFoundResult)));
+
+
+        }
+        [Test]
+        public async Task Details_IfIdDoesntExist_ReturnNotFoudResult()
+        {
+
+            var controller = new ItemsController(_context);
+            var result = await controller.Details(99);
+            Assert.That(result, Is.InstanceOf(typeof(NotFoundResult)));
+
+
+        }
+        [Test]
+        public async Task Details_IsReturnedModelHasSameValues_ReturnTrue()
+        {
+            var emp = _context.Items.Find(1);
+            var controller = new ItemsController(_context);
+            var vr = await controller.Details(1) as ViewResult;
+            var result = vr.ViewData.Model as Item;
+            Assert.IsTrue(result.Id == emp.Id && result.Name == emp.Name);
+
+
+
+        }
+        [Test]
+        public async Task EditGet_CheckRecordWithExistingKey_ReturnViewResult()
+        {
+            var controller = new ItemsController(_context);
+            var result = await controller.Edit(1) as ViewResult;
+            Assert.IsNotNull(result);
+
+
+        }
+        [Test]
+        public async Task EditGet_CheckRecordWithNull_ReturnNotFoundResult()
+        {
+
+            var controller = new ItemsController(_context);
+            var result = await controller.Edit(null);
+            Assert.That(result, Is.InstanceOf(typeof(NotFoundResult)));
+
+
+        }
+        [Test]
+        public async Task EditPost_EditRecordWithNoExistingId_ReturnNotFoundResult()
+        {
+            var emp = _context.Items.Find(1);
+            var controller = new ItemsController(_context);
+            var result = await controller.Edit(99, emp);
+            Assert.That(result, Is.InstanceOf(typeof(NotFoundResult)));
+
+
+        }
+        [Test]
+        public async Task EditPost_EditRecordWithDifferentId_ReturnNotFoundResult()
+        {
+            var emp = _context.Items.Find(2);
+            var controller = new ItemsController(_context);
+            var result = await controller.Edit(1, emp);
+            Assert.That(result, Is.InstanceOf(typeof(NotFoundResult)));
+
+
+        }
+        [Test]
+        public async Task DeleteGet_CheckRecordWithExistingKey_ReturnViewResult()
+        {
+            var controller = new ItemsController(_context);
+            var result = await controller.Delete(1) as ViewResult;
+            Assert.IsNotNull(result);
+
+
+        }
+        [Test]
+        public async Task DeleteGet_CheckRecordWithNotExistingKey_ReturnNull()
+        {
+
+            var controller = new ItemsController(_context);
+            var result = await controller.Delete(99) as ViewResult;
+            Assert.IsNull(result);
+
+
+        }
+        [Test]
+        public async Task DeleteGet_CheckRecordWithNull_ReturnNotFoundResult()
+        {
+
+            var controller = new ItemsController(_context);
+            var result = await controller.Delete(null);
+            Assert.That(result, Is.InstanceOf(typeof(NotFoundResult)));
+
+
+        }
+        [Test]
+        public async Task DeleteGet_IfIdDoesntExist_ReturnNotFoudResult()
+        {
+
+            var controller = new ItemsController(_context);
+            var result = await controller.Delete(99);
+            Assert.That(result, Is.InstanceOf(typeof(NotFoundResult)));
+
+
+        }
+        [Test]
+        public async Task DeletePost_RedirectToIndexIfCorrectData_ReturnTrue()
+        {
+            var _options = new DbContextOptionsBuilder<DotNetWMSContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            DotNetWMSContext _context = new DotNetWMSContext(_options);
+            _context.Database.EnsureCreated();
+            Initialize(_context);
+
+            var emp = new Item() { Id = 4, Type = "Komputer osobisty", Name = "Laptop", Producer = "Lenovo", Model = "X1 Carbon", ItemCode = "2", Quantity = 1, Units = 0, WarrantyDate = DateTime.Now, State = 0, EmployeeId = 2, WarehouseId = 2, ExternalId = 1 };
+            var controller = new ItemsController(_context);
+            await controller.Create(emp);
+            var result = await controller.DeleteConfirmed(4) as RedirectToActionResult;
+            Assert.IsTrue(result.ActionName == nameof(controller.Index));
+
+
+        }
+    }
+}
---
+++ DotNetWMSTests/DotNetWMSTests_Selenium_Adam.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotNetWMS.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DotNetWMS.Models;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using DotNetWMSTests.Selenium_test;
+using System.Threading;
+
+namespace DotNetWMSTests
+{
+    [TestFixture]
+    public class DotNetWMSTests_Selenium_Adam
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void AddItem()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/");
+            driver.FindElement(By.XPath("(//a[@type='button'])[3]")).Click();
+            driver.FindElement(By.LinkText("Create New")).Click();
+            driver.FindElement(By.Id("Type")).Click();
+            driver.FindElement(By.Id("Type")).Clear();
+            driver.FindElement(By.Id("Type")).SendKeys("Telefon");
+            driver.FindElement(By.Id("Name")).Clear();
+            driver.FindElement(By.Id("Name")).SendKeys("Telefon");
+            driver.FindElement(By.Id("Producer")).Clear();
+            driver.FindElement(By.Id("Producer")).SendKeys("Apple");
+            driver.FindElement(By.Id("Model")).Clear();
+            driver.FindElement(By.Id("Model")).SendKeys("Iphone 11 pro max");
+            driver.FindElement(By.Id("ItemCode")).Clear();
+            driver.FindElement(By.Id("ItemCode")).SendKeys("1");
+            driver.FindElement(By.Id("Quantity")).Clear();
+            driver.FindElement(By.Id("Quantity")).SendKeys("1");
+            driver.FindElement(By.Id("WarrantyDate")).Click();
+            driver.FindElement(By.Id("WarrantyDate")).Click();
+            driver.FindElement(By.Id("WarrantyDate")).Click();
+            driver.FindElement(By.Id("WarrantyDate")).Click();
+            driver.FindElement(By.Id("WarrantyDate")).Click();
+            // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | id=WarrantyDate | ]]
+            driver.FindElement(By.Id("WarrantyDate")).Clear();
+            driver.FindElement(By.Id("WarrantyDate")).SendKeys("0002-01-01");
+            driver.FindElement(By.Id("WarrantyDate")).Clear();
+            driver.FindElement(By.Id("WarrantyDate")).SendKeys("0020-01-01");
+            driver.FindElement(By.Id("WarrantyDate")).Clear();
+            driver.FindElement(By.Id("WarrantyDate")).SendKeys("0202-01-01");
+            driver.FindElement(By.Id("WarrantyDate")).Clear();
+            driver.FindElement(By.Id("WarrantyDate")).SendKeys("2022-01-01");
+            driver.FindElement(By.Id("WarrantyDate")).Clear();
+            driver.FindElement(By.Id("WarrantyDate")).SendKeys("20222-01-01");
+            driver.FindElement(By.Id("State")).Click();
+            new SelectElement(driver.FindElement(By.Id("State"))).SelectByText("Nowy");
+            driver.FindElement(By.Id("State")).Click();
+            driver.FindElement(By.Id("EmployeeId")).Click();
+            new SelectElement(driver.FindElement(By.Id("EmployeeId"))).SelectByText("Testowa Janusz");
+            driver.FindElement(By.Id("EmployeeId")).Click();
+            driver.FindElement(By.Id("WarehouseId")).Click();
+            new SelectElement(driver.FindElement(By.Id("WarehouseId"))).SelectByText("Pawia");
+            driver.FindElement(By.Id("WarehouseId")).Click();
+            driver.FindElement(By.Id("ExternalId")).Click();
+            new SelectElement(driver.FindElement(By.Id("ExternalId"))).SelectByText("Brak");
+            driver.FindElement(By.Id("ExternalId")).Click();
+            driver.FindElement(By.Id("WarrantyDate")).Click();
+            driver.FindElement(By.Id("WarrantyDate")).Clear();
+            driver.FindElement(By.Id("WarrantyDate")).SendKeys("2022-01-01");
+            driver.FindElement(By.XPath("//input[@value='Create']")).Click();
+            driver.FindElement(By.Id("ItemCode")).Click();
+            driver.FindElement(By.Id("ItemCode")).Clear();
+            driver.FindElement(By.Id("ItemCode")).SendKeys("5");
+            driver.FindElement(By.XPath("//input[@value='Create']")).Click();
+            Assert.IsTrue(IsElementPresent(By.XPath("//tr[2]/td[4]")));
+            // ERROR: Caught exception [unknown command []]
+            // ERROR: Caught exception [unknown command []]
+        driver.Navigate().GoToUrl("https://localhost:44387/Items");
+        driver.FindElement(By.XPath("(//a[contains(text(),'Edit')])[2]")).Click();
+        driver.FindElement(By.Id("Model")).Click();
+        driver.FindElement(By.Id("Model")).Clear();
+        driver.FindElement(By.Id("Model")).SendKeys("Iphone 11 pro");
+        driver.FindElement(By.XPath("//input[@value='Save']")).Click();
+        Assert.IsTrue(IsElementPresent(By.XPath("//tr[2]/td[4]")));
+        // ERROR: Caught exception [unknown command []]
+    }
+        [Test]
+        public void DetailItem()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/Items");
+            driver.FindElement(By.XPath("(//a[contains(text(),'Details')])[2]")).Click();
+            Assert.IsTrue(IsElementPresent(By.XPath("//main/div")));
+        }
+
+        [Test]
+        public void AssignToEmployee()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/");
+            driver.FindElement(By.XPath("//div[2]/div[2]/div[2]/div[2]/a/i")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Assign')])[2]")).Click();
+            driver.FindElement(By.Id("EmployeeId")).Click();
+            new SelectElement(driver.FindElement(By.Id("EmployeeId"))).SelectByText("Testowa Janusz");
+            driver.FindElement(By.Id("EmployeeId")).Click();
+            driver.FindElement(By.XPath("//input[@value='Save']")).Click();
+            driver.FindElement(By.LinkText("Back to List")).Click();
+            // ERROR: Caught exception [unknown command []]
+        }
+
+        [Test]
+        public void AssignToWarehouse()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/");
+            driver.FindElement(By.XPath("//div[3]/div[2]/a/i")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Assign')])[2]")).Click();
+            driver.FindElement(By.Id("WarehouseId")).Click();
+            new SelectElement(driver.FindElement(By.Id("WarehouseId"))).SelectByText("Główny, Borsucza");
+            driver.FindElement(By.Id("WarehouseId")).Click();
+            driver.FindElement(By.XPath("//input[@value='Save']")).Click();
+            // ERROR: Caught exception [unknown command []]
+        }
+
+        [Test]
+        public void AssignToExternal()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/");
+            driver.FindElement(By.XPath("//div[4]/div[2]/a/i")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Assign')])[2]")).Click();
+            driver.FindElement(By.Id("ExternalId")).Click();
+            new SelectElement(driver.FindElement(By.Id("ExternalId"))).SelectByText("Brak");
+            driver.FindElement(By.Id("ExternalId")).Click();
+            driver.FindElement(By.XPath("//input[@value='Save']")).Click();
+            Assert.IsTrue(IsElementPresent(By.XPath("//tr[2]/td[8]")));
+            // ERROR: Caught exception [unknown command []]
+        }
+        [Test]
+        public void DeleteItem()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/Items");
+            driver.FindElement(By.XPath("(//a[contains(text(),'Delete')])[2]")).Click();
+            driver.FindElement(By.XPath("//input[@value='Delete']")).Click();
+            Assert.IsFalse(IsElementPresent(By.XPath("//tr[2]/td[4]")));
+        }
+        [Test]
+        public void NewWarehouse()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/");
+            driver.FindElement(By.XPath("(//a[@type='button'])[7]")).Click();
+            driver.FindElement(By.LinkText("Create New")).Click();
+            driver.FindElement(By.Id("Name")).Click();
+            driver.FindElement(By.Id("Name")).Clear();
+            driver.FindElement(By.Id("Name")).SendKeys("Nowy Magazyn");
+            driver.FindElement(By.Id("Street")).Clear();
+            driver.FindElement(By.Id("Street")).SendKeys("Sezamkowa");
+            driver.FindElement(By.Id("ZipCode")).Clear();
+            driver.FindElement(By.Id("ZipCode")).SendKeys("31-011");
+            driver.FindElement(By.Id("City")).Clear();
+            driver.FindElement(By.Id("City")).SendKeys("Kraków");
+            driver.FindElement(By.XPath("//input[@value='Create']")).Click();
+            Assert.IsTrue(IsElementPresent(By.XPath("//tr[5]/td")));
+            // ERROR: Caught exception [unknown command []]
+        }
+        [Test]
+        public void EditWarehouse()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/Warehouses");
+            driver.FindElement(By.XPath("(//a[contains(text(),'Edit')])[5]")).Click();
+            driver.FindElement(By.Id("Street")).Click();
+            driver.FindElement(By.Id("Street")).Click();
+            // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | id=Street | ]]
+            driver.FindElement(By.Id("Street")).Clear();
+            driver.FindElement(By.Id("Street")).SendKeys("Rozrywki");
+            driver.FindElement(By.XPath("//input[@value='Save']")).Click();
+            Assert.IsTrue(IsElementPresent(By.XPath("//tr[5]/td[2]")));
+        }
+        [Test]
+        public void DetailWarehouse()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/Warehouses");
+            driver.FindElement(By.XPath("(//a[contains(text(),'Details')])[5]")).Click();
+            Assert.IsTrue(IsElementPresent(By.XPath("//dd")));
+
+        }
+        [Test]
+        public void DeleteWarehouse()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/Warehouses");
+            driver.FindElement(By.XPath("(//a[contains(text(),'Delete')])[5]")).Click();
+            driver.FindElement(By.XPath("//input[@value='Delete']")).Click();
+            Assert.IsFalse(IsElementPresent(By.XPath("//tr[5]/td")));
+            
+        }
+        [Test]
+        public void Stocktaking()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/");
+            driver.FindElement(By.XPath("//button/i")).Click();
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+
+
+    }
+}
