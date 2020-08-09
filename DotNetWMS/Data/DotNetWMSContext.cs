5dded6d4cb6ca9d30754a227a5b60717a08488c9
mjKeszely <kamil.szydlowski@microsoft.wsei.edu.pl>
2020-08-09 23:23:00 +0200

    Code documentation - others

--- DotNetWMS/Data/DotNetWMSContext.cs
+++ DotNetWMS/Data/DotNetWMSContext.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 
 namespace DotNetWMS.Data
 {
+    /// <summary>
+    /// <para>Class associated with the Entity Core framework, responsible for creating databases related to the appropriate classes in the Models directory.</para> 
+    /// <para>The Data Annotattions method was used to pass the field requirements.</para>
+    /// </summary>
     public class DotNetWMSContext : IdentityDbContext<WMSIdentityUser>
     {
         public DotNetWMSContext(DbContextOptions<DotNetWMSContext> options) : base(options)
@@ -21,10 +25,16 @@ namespace DotNetWMS.Data
         public DbSet<Department> Departments { get; set; }
         public DbSet<External> Externals { get; set; }
 
+        /// <summary>
+        /// The method is used to store the database creation settings
+        /// </summary>
+        /// <param name="modelBuilder">ModelBuilder class object</param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            //A code that prevents the deletion of data that are linked with foreign keys. 
+            //It is necessary to remove dependencies before deletion.
             foreach (var foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
             {
                 foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
--- DotNetWMS/Resources/RoleAssignExtension.cs
+++ DotNetWMS/Resources/RoleAssignExtension.cs
@@ -6,13 +6,27 @@ using System.Threading.Tasks;
 
 namespace DotNetWMS.Resources
 {
-    public static class RoleAssignExtension
+	/// <summary>
+	/// Static class used in views which access requires authorization, to check whether the user has been assigned to the role authorizing him to run a given view
+	/// </summary>
+	public static class RoleAssignExtension
     {
+		/// <summary>
+		/// The method checks that the user is assigned to all provided roles
+		/// </summary>
+		/// <param name="principal">Interface for checking if the user is assigned to the provided role</param>
+		/// <param name="roles">User-assigned roles in the application</param>
+		/// <returns>Returns true if the user is assigned to each role. Otherwise - returns false</returns>
 		public static bool IsInAllRoles(this IPrincipal principal, params string[] roles)
 		{
 			return roles.All(r1 => r1.Split(',').All(r2 => principal.IsInRole(r2.Trim())));
 		}
-
+		/// <summary>
+		/// The method checks that the user is assigned to any provided roles
+		/// </summary>
+		/// <param name="principal">Interface for checking if the user is assigned to the provided role</param>
+		/// <param name="roles">User-assigned roles in the application</param>
+		/// <returns>Returns true if the user is assigned to any role. Otherwise - returns false</returns>
 		public static bool IsInAnyRoles(this IPrincipal principal, params string[] roles)
 		{
 			return roles.Any(r1 => r1.Split(',').Any(r2 => principal.IsInRole(r2.Trim())));
