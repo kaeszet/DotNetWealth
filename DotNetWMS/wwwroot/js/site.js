c7f23aecec457c04c796fb695ec5549878e99f5f
Krzysztof <krzysztof@webfalcon.pl>
2021-01-14 21:28:44 +0100

    update

--- DotNetWMS/wwwroot/js/site.js
+++ DotNetWMS/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function ()
+{
+    //Active nav list
+    $('.sidebar .list-group-item[href="' + location.pathname + '"]').addClass('active');
+
+    //Input search table
+    $(".search-control").on("keyup", function ()
+    {
+        var value = $(this).val().toLowerCase();
+        $("#tableSearch tr").filter(function ()
+        {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
