3f0c1a3c05740e956283f5f449c158f1ecdaf21b
Krzysztof <krzysztofwcislo1234@gmail.pl>
2021-02-13 22:16:54 +0100

    update color and logo

--- DotNetWMS/wwwroot/js/site.js
+++ DotNetWMS/wwwroot/js/site.js
@@ -12,7 +12,27 @@ $(document).ready(function ()
     });
 
     //Active nav list
-    $('.sidebar .list-group-item[href="' + location.pathname + '"]').addClass('active');
+    //$('.sidebar .list-group-item[href="' + location.pathname + '"]').addClass('active');
+
+    if ($('.sidebar').length > 0)
+    {
+
+        $('.sidebar .list-group-item').each(function () {
+
+            if ($(this).attr('href') == location.pathname) {
+                localStorage.setItem('active', $(this).data('id'))
+            }
+
+            if ($(this).data('id') == localStorage.getItem('active'))
+            {
+                $(this).addClass('active');
+            }
+
+            $(this).on('click', function () {
+                localStorage.setItem('active', $(this).data('id'))
+            })
+        })
+    }
 
     //Input search table
     $(".search-control").on("keyup", function ()
@@ -52,4 +72,47 @@ $(document).ready(function ()
             document.cookie = $(this).data('cookie-string');
         });
     }
+
+
 });
+
+(function ($)
+{
+    $.fn.myfunction = function () {
+        alert('hello world');
+        return this;
+    };
+
+})(jQuery);
+
+function initMap()
+{
+    console.log('asd')
+    var myCenter = new google.maps.LatLng(50.046912, 19.998207);
+    var mapProp = { center: myCenter, zoom: 12, scrollwheel: false, draggable: true, mapTypeId: google.maps.MapTypeId.ROADMAP };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var marker = new google.maps.Marker({ position: myCenter });
+
+    marker.setMap(map);
+
+    const geocoder = new google.maps.Geocoder();
+    geocodeAddress(geocoder, map);
+}
+
+function geocodeAddress(geocoder, resultsMap)
+{
+    const address = adressToGeoCode;
+    geocoder.geocode({ address: address }, (results, status) => {
+        if (status === "OK") {
+            resultsMap.setCenter(results[0].geometry.location);
+            new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location,
+            });
+        } else {
+            alert(
+                "Wystąpił błąd usługi Geocode z powodu: " + status
+            );
+        }
+    });
+}
